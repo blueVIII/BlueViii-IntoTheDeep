d433b40083a565aa6a78f3a0bf2a8d6235647e65
Kevin Quan <kcq@yahoo.com>
2025-03-21 12:19:59 -0400

    updated FTC SDK 10.2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueSampleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueSampleAuto.java
@@ -13,6 +13,7 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Config
